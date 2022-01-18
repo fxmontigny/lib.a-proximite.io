4399409ac0785d39233bed08b201f871a5624636
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-01-18 16:31:14 +0100

    update header and footer

--- components/site-header/site-header.component.ts
+++ components/site-header/site-header.component.ts
@@ -1,21 +1,38 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnInit } from '@angular/core';
-import { MainClass } from 'lib/libs/main-class';
+import { DOCUMENT } from "@angular/common";
+import { Component, HostBinding, HostListener, Inject, AfterViewInit } from "@angular/core";
+import { MainClass } from "lib/libs/main-class";
 
 @Component({
-	selector: 'site-header',
-	templateUrl: './site-header.component.html',
-	styleUrls: [ './site-header.component.scss' ]
+  selector: "site-header",
+  templateUrl: "./site-header.component.html",
+  styleUrls: ["./site-header.component.scss"],
 })
-export class SiteHeaderComponent extends MainClass implements OnInit {
-	showPopupMenu: boolean = false;
-	constructor(@Inject(DOCUMENT) private document: any) {
-		super();
+export class SiteHeaderComponent extends MainClass implements AfterViewInit {
+  showPopupMenu: boolean = false;
+	@HostBinding('class.is-scrolled') isScrolled = false;
+
+  constructor(@Inject(DOCUMENT) private document: any) {
+    super();
+  }
+
+  ngAfterViewInit() {
+		this.updateHeaderStyle();
 	}
 
-	ngOnInit() {}
+  isActive(path) {
+    return this.document.location.href === path;
+  }
 
-	isActive(path) {
-		return this.document.location.href === path;
+  @HostListener("window:scroll", ["$event"]) // for window scroll events
+  onScroll() {
+    this.updateHeaderStyle();
+  }
+
+  updateHeaderStyle() {
+		if(window.scrollY) {
+			this.isScrolled = true;
+		} else {
+			this.isScrolled = false;
+		}
 	}
 }
